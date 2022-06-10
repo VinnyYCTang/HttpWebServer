50920c7e912e2b3b9be83ccf7df1c679c4868e84
VinnyYCTang <vinny.yc.tang@gmail.com>
2022-06-10 17:00:27 +0800

    create server socket and bind to port 8080.

--- src/main/java/com/example/httpwebserver/core/BootStrap.java
+++ src/main/java/com/example/httpwebserver/core/BootStrap.java
@@ -1,5 +1,10 @@
 package com.example.httpwebserver.core;
 
+import com.example.httpwebserver.util.Logger;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+
 /*
 * httpServer main program entrance
 * @author Vinny.YC.Tang
@@ -16,6 +21,24 @@ public class BootStrap {
     }
 
     public static void start() {
-
+        long begin = System.currentTimeMillis();
+        Logger.log("httpserver start");
+        ServerSocket serverSocket = null;
+        try {
+            int port = 8080;
+            serverSocket = new ServerSocket(port);
+            long end = System.currentTimeMillis();
+            Logger.log("httpserver started, " + (end-begin) + "ms");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(serverSocket != null && !serverSocket.isClosed()){
+                try {
+                    serverSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
     }
 }
