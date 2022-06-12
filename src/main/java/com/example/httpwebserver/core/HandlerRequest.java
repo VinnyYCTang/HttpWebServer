08f78a929939d0aa82bf7289dab5deeadbc64721
VinnyYCTang <vinny.yc.tang@gmail.com>
2022-06-12 15:39:51 +0800

    response static resources to client.

--- src/main/java/com/example/httpwebserver/core/HandlerRequest.java
+++ src/main/java/com/example/httpwebserver/core/HandlerRequest.java
@@ -1,10 +1,7 @@
 package com.example.httpwebserver.core;
 
 import com.example.httpwebserver.util.Logger;
-
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.Socket;
 
 /*
@@ -25,12 +22,22 @@ public class HandlerRequest implements Runnable{
     public void run() {
 
         BufferedReader br = null;
+        PrintWriter out = null;
         Logger.log("httpServer thread: " + Thread.currentThread().getName());
         try {
             br = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+
+
+            out = new PrintWriter(clientSocket.getOutputStream());
+
+
             String requestLine = br.readLine();// GET resource.html HTTP/1.1
             String requestURI = requestLine.split(" ")[1];
-            System.out.println(requestURI);
+            // Check user's request is static resource or not.
+            if(requestURI.endsWith(".html") || requestURI.endsWith(".htm")){
+                responseStaticPage(requestURI, out);
+            }
+            out.flush();
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
@@ -44,6 +51,55 @@ public class HandlerRequest implements Runnable{
             if (clientSocket != null && !clientSocket.isClosed()) {
                 try {
                     clientSocket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    /*
+    * Deal with the static resource which URI points to.
+    * @param String requestURI
+    * @param PrintWriter out
+    * */
+    public void responseStaticPage(String requestURI, PrintWriter out) {
+
+        // requestURI : /welcome/index.html
+        // static resource path : welcome/index.html
+        String htmlPath = requestURI.substring(1);
+        BufferedReader br = null;
+        try {
+            br = new BufferedReader(new FileReader(htmlPath));
+            StringBuilder html = new StringBuilder();
+            html.append("HTTP/1.1 200 OK\n");
+            html.append("Content-Type:text/html;charset=utf-8\n\n");
+            String temp = null;
+            while((temp = br.readLine()) != null){
+                html.append(temp);
+            }
+            out.print(html);
+        } catch (FileNotFoundException e) {
+            // 404 NotFound
+            StringBuilder html = new StringBuilder();
+            html.append("HTTP/1.1 404 NotFound\n");
+            html.append("Content-Type:text/html;charset=utf-8\n\n");
+            html.append("<html>");
+            html.append("<head>");
+            html.append("<title>404-錯誤</title>");
+            html.append("<meta content='text/html;charset=utf-8' />");
+            html.append("</head>");
+            html.append("<body>");
+            html.append("<H1 style='color:red'>404錯誤</H1>");
+            html.append("</body>");
+            html.append("</html>");
+            out.print(html);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }finally {
+            if(br != null){
+                try {
+                    br.close();
                 } catch (IOException e) {
                     e.printStackTrace();
                 }
