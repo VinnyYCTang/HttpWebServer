8dc3c3e3b7edd70413973dc84fd78d350e7aa1d6
VinnyYCTang <vinny.yc.tang@gmail.com>
2022-06-14 17:09:56 +0800

    encapsulate a request object.

---
+++ src/main/java/com/example/httpwebserver/core/RequestObject.java
@@ -0,0 +1,64 @@
+package com.example.httpwebserver.core;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+/*
+ * encapsulate a request object.
+ * @author Vinny.YC.Tang
+ * @version  1.0
+ * @since    1.0
+ *
+ * */
+public class RequestObject {
+
+    private final Map<String, List<String>> parameterMap = new HashMap<>();
+
+    public RequestObject(String requestURI){
+
+        List<String> values = null;
+        if(requestURI.contains("?")){
+            String[] uriAndData = requestURI.split("[?]");
+            if(uriAndData.length > 1) {
+                String data = uriAndData[1];
+                if (data.contains("&")) {
+                    // multiple parameters
+                    String[] nameAndValues = data.split("&");
+                    for(String nameAndValue : nameAndValues){
+                        values = new ArrayList<>();
+                        String[] nameAndValueAttr = nameAndValue.split("=");
+                        if(parameterMap.containsKey(nameAndValueAttr[0])){
+                            if(nameAndValueAttr.length > 1)
+                                parameterMap.get(nameAndValueAttr[0]).add(nameAndValueAttr[1]);
+                        }else{
+                            if(nameAndValueAttr.length > 1)
+                               values.add(nameAndValueAttr[1]);
+                            parameterMap.put(nameAndValueAttr[0], values);
+                        }
+                    }
+                } else {
+                    // single parameter
+                    String[] nameAndValue = data.split("=");
+                    values = new ArrayList<>();
+                    if(nameAndValue.length > 1)
+                        values.add(nameAndValue[1]);
+                    parameterMap.put(nameAndValue[0], values);
+                }
+            }
+        }
+
+    }
+
+    public String getParameterValue(String key){
+        ArrayList<String> list = (ArrayList<String>) parameterMap.get(key);
+        return (list != null && list.size() != 0) ? list.get(0) : null;
+    }
+
+    public String[] getParameterValues(String key) {
+        String[] value = new String[]{};
+        return parameterMap.get(key).toArray(value);
+    }
+
+}
